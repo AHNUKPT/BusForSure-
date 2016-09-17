fead29b837ee75b2ed109c3cb3f8218f444ff5fd
HIMANSHU SHEKHAR <HIMANSHUSHEKH123@GMAIL.COM>
2016-09-17 17:08:56 +0530

    Create myscript.js

---
+++ 30-08/myscript.js
@@ -0,0 +1,45 @@
+<script>
+
+  var x;
+
+  var db = openDatabase('route', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+  db.transaction(function (tx) {
+  
+    tx.executeSql('CREATE TABLE IF NOT EXISTS buses (busroute unique, busno)');
+    
+    tx.executeSql('INSERT INTO buses (busroute, busno) VALUES (1, "5586")');
+    tx.executeSql('INSERT INTO buses (busroute, busno) VALUES (2, "4158")');
+    tx.executeSql('INSERT INTO buses (busroute, busno) VALUES (3, "7485")');
+    tx.executeSql('INSERT INTO buses (busroute, busno) VALUES (4, "9856")');
+    
+  });
+
+  select=document.getElementById("select");
+
+  select.onchange=function(){
+
+    x = this.value;
+
+    db.transaction(function (tx) {
+    
+      tx.executeSql('SELECT * FROM buses',[], function (tx,results) {
+      
+        var len = results.rows.length, i;
+
+        for(i=0;i<len;i++){
+        
+          if(x==results.rows.item(i).busroute){
+            var y=results.rows.item(i).busno;
+            document.getElementById("busNo").innerHTML=y;
+            
+          }
+        }
+
+      });
+      
+    });
+
+  }
+
+</script>
